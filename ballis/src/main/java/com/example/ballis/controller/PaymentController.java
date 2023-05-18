ab435db1f8b737d558ad9ee55b148296c334e5c9
Hyun Jeong Shin <hj0304hj@gmail.com>
2023-05-18 11:57:52 +0900

    카카오 결제 구현 완료

--- ballis/src/main/java/com/example/ballis/controller/PaymentController.java
+++ ballis/src/main/java/com/example/ballis/controller/PaymentController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ballis.DTO.ContractAddDTO;
-import com.example.ballis.DTO.ContractAndPaymentDTO;
+import com.example.ballis.DTO.ContractSellingPaymentDTO;
 import com.example.ballis.DTO.PaymentAddDTO;
+import com.example.ballis.DTO.SellingAddDTO;
 import com.example.ballis.model.Contract;
 import com.example.ballis.model.Payment;
+import com.example.ballis.model.Selling;
 import com.example.ballis.service.BuyingService;
 import com.example.ballis.service.ContractService;
 import com.example.ballis.service.PaymentService;
@@ -32,41 +34,44 @@ public class PaymentController {
 	@Autowired
 	private BuyingService buyingService;
 	
-	// 결제 완료
+	// 거래 체결 후 결제 완료 -> 빠른배송, 즉시구매, 보관판매
 	@PostMapping("/api/add/payment")
-    public ResponseEntity<Payment> addPayment(@RequestBody PaymentAddDTO paymentDto) {
-        try {
-            Payment paymentInfo = paymentService.addPayment(paymentDto);
-            return new ResponseEntity<>(paymentInfo, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-	
-	// 거래 체결 후 결제 완료
-	@PostMapping("/api/add/payment/test")
 	@Transactional
-	public ResponseEntity<ContractAndPaymentDTO> addConAndPay(@RequestBody ContractAndPaymentDTO conAndPayDto, @RequestParam String type) {
+	public ResponseEntity<ContractSellingPaymentDTO> addConSellPay(@RequestBody ContractSellingPaymentDTO conSellPayDto, @RequestParam String type) {
 	    try {
-	        ContractAddDTO contractDto = conAndPayDto.getContractDto();
-	        Contract contractInfo = contractService.save(contractDto, type);        
-	        sellingService.updateSelling(contractDto.getSellingId(), type); 
-	        
-	        PaymentAddDTO paymentDto = conAndPayDto.getPaymentDto(); 
-	        paymentDto.setContractId(contractInfo.getId());
-	        Payment paymentInfo = paymentService.addPayment(paymentDto);
-	        
-	        ContractAndPaymentDTO contractAndPaymentDTO = new ContractAndPaymentDTO();
-	        contractAndPaymentDTO.setContractDto(contractDto);
-	        contractAndPaymentDTO.setPaymentDto(paymentDto);
+	    	if("fast".equals(type) || "normal".equals(type)) { // 구매-빠른배송, 즉시구매
+		        ContractAddDTO contractDto = conSellPayDto.getContractDto();
+		        Contract contractInfo = contractService.save(contractDto, type);        
+		        sellingService.updateSelling(contractDto.getSellingId(), type);    	
+	    	
+		        PaymentAddDTO paymentDto = conSellPayDto.getPaymentDto(); 
+		        paymentDto.setContractId(contractInfo.getId());
+		        Payment paymentInfo = paymentService.addPayment(paymentDto);
+		        
+		        ContractSellingPaymentDTO contractAndPaymentDTO = new ContractSellingPaymentDTO();
+		        contractAndPaymentDTO.setContractDto(contractDto);
+		        contractAndPaymentDTO.setPaymentDto(paymentDto);
+		        return new ResponseEntity<>(contractAndPaymentDTO, HttpStatus.OK);
+		        
+	    	} else if("keep".equals(type)) { // 판매-보관판매
+		        SellingAddDTO sellingDto = conSellPayDto.getSellingDto();
+		        Selling sellingInfo = sellingService.save(sellingDto, type);
+	    	
+		        PaymentAddDTO paymentDto = conSellPayDto.getPaymentDto(); 
+		        paymentDto.setSellingId(sellingInfo.getId());
+		        Payment paymentInfo = paymentService.addPayment(paymentDto);
 
-	        return new ResponseEntity<>(contractAndPaymentDTO, HttpStatus.OK);		
+		        ContractSellingPaymentDTO contractAndPaymentDTO = new ContractSellingPaymentDTO();
+		        contractAndPaymentDTO.setSellingDto(sellingDto);
+		        contractAndPaymentDTO.setPaymentDto(paymentDto);
+		        return new ResponseEntity<>(contractAndPaymentDTO, HttpStatus.OK);
+		        
+		    } else {
+		    	return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		    }
 	    } catch (Exception e) {
 	        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 	}
-	
-
-
 	
 }
